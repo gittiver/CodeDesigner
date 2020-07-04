4f4e88817b6e158f04852ba6d21db2960ed4616a
Gulliver <gulliver@traumkristalle.net>
2020-07-04 17:30:39 +0200

    added cmake files for cmake based build on OSX

---
+++ release_it.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+HOST_SYSTEM=`uname -s`
+HOST_ARCH=`uname -m`
+STARTDIR=`pwd`
+PROJECTNAME=codedesigner
+
+CMAKE_BUILDDIR=build-$HOST_ARCH-$HOST_SYSTEM
+
+mkdir -p $CMAKE_BUILDDIR
+
+cd $CMAKE_BUILDDIR
+
+if [ "$HOST_SYSTEM" = "Linux" ] 
+then 
+cmake -D${PROJECTNAME}_WITH_DOCS:bool=ON \
+      ..
+
+cmake --build . --target all
+#cmake --build . --target test
+cmake --build . --target package 
+cmake --build . --target package_source
+cd $STARTDIR
+
+else 
+
+WXDIR=$STARTDIR/../wxWidgets/3.1.3
+
+cmake -D${PROJECTNAME}_WITH_DOCS:bool=ON \
+      -DwxWidgets_CONFIG_EXECUTABLE:FILEPATH=$WXDIR/build-cocoa-debug/wx-config \
+      ..
+
+cmake --build . --target all
+#cmake --build . --target test
+cmake --build . --target package 
+cmake --build . --target package_source
+cd $STARTDIR
+
+mkdir -p build-Xcode-$HOST_SYSTEM
+cd build-Xcode-$HOST_SYSTEM
+cmake -D${PROJECTNAME}_WITH_DOCS:bool=ON \
+      -DwxWidgets_CONFIG_EXECUTABLE:FILEPATH=$WXDIR/build-cocoa-debug/wx-config \
+      -G Xcode ..
+cd $STARTDIR
+
+fi
+
